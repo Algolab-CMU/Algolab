7077bdfb3ccfec6a916f680972d845793ad43089
Hanqi Sun <hanqi.sun@gmail.com>
2016-02-01 15:15:02 -0500

    Change models.py

--- basics/models.py
+++ basics/models.py
@@ -15,13 +15,6 @@ class Tag(models.Model):
     def __str__(self):
         return self.text
 
-class Comment(models.Model):
-    text = models.CharField(max_length=MAX_TEXT_LENGTH)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    mod_time = models.DateTimeField('time last modified')
-    parent = models.ForeignKey('self', null=True, blank=True) # whether this feed is a follow-up feed
-
-
 class Question(models.Model):
     title = models.CharField(max_length=MAX_TITLE_LENGTH)
     description = models.CharField(max_length=MAX_TEXT_LENGTH)
@@ -47,7 +40,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=MAX_TEXT_LENGTH)
     correctness = models.NullBooleanField()
-    comment = models.ManyToManyField(Comment)
     user_history = models.ManyToManyField(User)
 
     def __str__(self):
@@ -59,12 +51,16 @@ class Answer(models.Model):
     text = models.CharField(max_length=MAX_TEXT_LENGTH)
     correctness = models.NullBooleanField()
     mod_time = models.DateTimeField('time last modified')
-    comment = models.ManyToManyField(Comment)
     vote = models.IntegerField()
     contributer = models.ManyToManyField(User)
 
-
-
+class Comment(models.Model):
+    text = models.CharField(max_length=MAX_TEXT_LENGTH)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    mod_time = models.DateTimeField('time last modified')
+    parent = models.ForeignKey('self', null=True, blank=True) # whether this feed is a follow-up feed
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name="answer", default=1)
+    choice = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name="choice", default=1)
 
 class UserProfile(models.Model):
     realname = models.CharField(max_length=MAX_TITLE_LENGTH)
@@ -75,9 +71,9 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
-
 class Class(models.Model):
     name = models.CharField(max_length=MAX_TITLE_LENGTH)
     question = models.ManyToManyField(Question)
     instructor = models.ManyToManyField(User, related_name="instructors")
     student = models.ManyToManyField(User, related_name="students")
+    description = models.CharField(max_length=MAX_TEXT_LENGTH, default="")
