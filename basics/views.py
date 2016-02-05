a98d553dda24d5882003087c96f6fa6d4a0b1410
AllisonWang <allisonwang520@gmail.com>
2016-02-04 20:28:10 -0500

    add base_index.html

--- basics/views.py
+++ basics/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+from .models import Question
+
+def index(request):
+     return render(request, 'base_index.html', {})
