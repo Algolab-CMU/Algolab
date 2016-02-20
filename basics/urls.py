68bdb8434864b2595de289fb59bc9d3da89ffbb9
saraxiang <yiqix@andrew.cmu.edu>
2016-02-20 15:03:21 -0500

    home page

--- basics/urls.py
+++ basics/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'basics'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^home$', views.home, name='home'),
 ]
--- basics/views.py
+++ basics/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from basics.models import Tag, Question, Choice, Answer, Comment, UserProfile,
+Class
 # Create your views here.
 
 from django.shortcuts import render
@@ -8,3 +10,8 @@ from .models import Question
 
 def index(request):
      return render(request, 'basics/base_index.html', {})
+
+def home(request, username):
+     userprofile = UserProfile.objects.get(user__exact = username)
+     #classes_enrolled = Class.objects.filter()
+     return HttpResponse("This worked")
